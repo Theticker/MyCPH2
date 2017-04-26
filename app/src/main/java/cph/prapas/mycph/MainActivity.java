463e28464eaae91810d08a1b8e5dd94f9487a183
Prapas <Prapas1995@hotmail.com>
2017-04-26 12:47:44 +0700

    initial view ok

--- app/src/main/java/cph/prapas/mycph/MainActivity.java
+++ app/src/main/java/cph/prapas/mycph/MainActivity.java
@@ -1,12 +1,15 @@
 package cph.prapas.mycph;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuAdapter;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // Explicit
 
@@ -23,7 +26,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // controller
+        controller();
+
     } // mail method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+    }
+
 
     private void initialView() {
 
@@ -36,4 +48,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View v) {
+
+        // for textView
+
+        if (v == textView) {
+            //intent to Signup
+            Intent intent = new Intent(MainActivity.this,SigniActivity.class);
+            startActivity(intent);
+
+        }
+
+        // For button
+        if (v == button) {
+
+        }
+    }
 }// main class
